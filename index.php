0a12da9236abc55e6053a0af7988b57d35500f60
Marcos Rivera <mrivera22@mail.greenriver.edu>
2020-01-10 13:47:41 -0500

    Added a view for project that is a home page

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 $f3->route('GET /', function() {
-    echo "<h1>Howdy!</h1>";
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 $f3->run();
